d8a28d46b66284bdb5962b6f01bf5939fef7c9e2
vladimir <levranzz@gmail.com>
2018-04-26 23:37:25 +0300

    first commit

--- DZ3/ViewController.swift
+++ DZ3/ViewController.swift
@@ -15,8 +15,9 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
-
+    func <#name#>(<#parameters#>) -> <#return type#> {
+        <#function body#>
+    }
 
 }
 
